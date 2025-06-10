04ef1d9a1d479a9dfea340005a230280250b105e
Julio Leon <juliopena143@icloud.com>
2025-06-10 00:55:27 -0600

    login page

--- ai-cloud-document-platform/src/App.jsx
+++ ai-cloud-document-platform/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import LoginForm from './pages/Login.jsx'
 
-export const App = () => {
-  return (
-    <div>App</div>
+
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const App = () => {
+  const user = true
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/login" element={!user ? <LoginForm /> : <Navigate to="/" />} />
+  </Routes>
+  </BrowserRouter>
+
   )
 }
+
+
+export default App
--- ai-cloud-document-platform/src/pages/Login.jsx
+++ ai-cloud-document-platform/src/pages/Login.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 
-export default function Login() {
+export default function LoginForm() {
+    const [isLoginForm, setLoginForm] = useState(true);
 
     return (
-        <div className="login">
-            
+        <div className="w-[430px] bg-white p-8 rounded-2xl shadow-lg">
+            <div className="flex justify-center mb-4">
+                <h2 className="text-3xl font-semibold text-center">
+                    {isLoginForm ? "Login" : "Sign Up"}
+                </h2>
+            </div>
+
+            {/* Tab Controls */}
+            <div className="relative flex h-12 mb-6 border border-gray-300 rounded-full overflow-hidden"></div>
+
         </div>
     )
 }
