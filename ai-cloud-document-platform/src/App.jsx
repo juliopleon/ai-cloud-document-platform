39172f1b2dd60977229f717aefa5c20edc3c144e
Julio Leon <juliopena143@icloud.com>
2025-06-10 01:29:45 -0600

    loginform styling

--- ai-cloud-document-platform/src/App.jsx
+++ ai-cloud-document-platform/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LoginForm from './pages/Login.jsx'
+import HomeDashBoard from "./pages/Dashboard.jsx"
 
 
 import {
@@ -11,9 +12,10 @@ import {
 } from "react-router-dom";
 
 const App = () => {
-  const user = true
+  const user = false
   return (<BrowserRouter>
   <Routes>
+    <Route path="/*" element={user ? <HomeDashBoard /> : <Navigate to="/login" />} />
     <Route path="/login" element={!user ? <LoginForm /> : <Navigate to="/" />} />
   </Routes>
   </BrowserRouter>
--- ai-cloud-document-platform/src/pages/Login.jsx
+++ ai-cloud-document-platform/src/pages/Login.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 
 export default function LoginForm() {
-    const [isLoginForm, setLoginForm] = useState(true);
+    const [isLoginMode, setIsLoginMode] = useState(true);
 
     return (
         <div className="w-[430px] bg-white p-8 rounded-2xl shadow-lg">
             <div className="flex justify-center mb-4">
                 <h2 className="text-3xl font-semibold text-center">
-                    {isLoginForm ? "Login" : "Sign Up"}
+                    {isLoginMode ? "Login" : "Sign Up"}
                 </h2>
             </div>
 
             {/* Tab Controls */}
-            <div className="relative flex h-12 mb-6 border border-gray-300 rounded-full overflow-hidden"></div>
+            <div className="relative flex h-12 mb-6 border border-gray-300 rounded-full overflow-hidden">
+                <button
+                className={`w-1/2 text-lg font-medium transition-all z-10 ${
+                    isLoginMode ? "text-white" : "text-black"}`}
+                onClick={() => setIsLoginMode(true)} 
+                >
+                    Login
+                </button>
+            </div>
 
         </div>
     )
